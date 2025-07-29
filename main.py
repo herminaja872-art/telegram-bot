13e598ab6a00e0143216dd660229782ecf614059
herminaja872-art <herminaja872@gmail.com>
2025-07-29 23:13:01 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import telebot
+import os
+from flask import Flask, request
+
+API_TOKEN = os.environ.get('BOT_TOKEN')
+bot = telebot.TeleBot(API_TOKEN)
+app = Flask(__name__)
+
+@app.route('/' + API_TOKEN, methods=['POST'])
+def getMessage():
+    json_str = request.get_data().decode('UTF-8')
+    update = telebot.types.Update.de_json(json_str)
+    bot.process_new_updates([update])
+    return "OK", 200
+
+@app.route("/", methods=['GET'])
+def webhook():
+    return "Bot is alive!"
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "Hello! I'm your EasyCoinsBot.")
