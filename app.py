bded2e4658a7d327ba142753e4a7c00d94591383
herminaja872-art <herminaja872@gmail.com>
2025-07-30 14:16:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, request
 import telebot
+import os
 
-TOKEN = "8324131113:AAFQbIs_LAIe08c1xMFP75q1eFltASLHvNA"
+TOKEN = "8324131113:AAFQbIs_LAIe08c1..."
 bot = telebot.TeleBot(TOKEN)
 
 WEBHOOK_URL = f"https://telegram-bot-qgf5.onrender.com/{TOKEN}"
@@ -13,12 +14,12 @@ app = Flask(__name__)
 # Odpoveď na /start
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.reply_to(message, "✅ Hello! Bot is working.")
+    bot.reply_to(message, "✅ Hello!")
 
 # Webhook handler
 @app.route(f'/{TOKEN}', methods=['POST'])
 def webhook():
-    update = telebot.types.Update.de_json(request.get_json(force=True))
+    update = telebot.types.Update.de_json(request.stream.read().decode("utf-8"))
     bot.process_new_updates([update])
     return "OK", 200
 
@@ -28,4 +29,4 @@ def index():
     return "Bot is running!", 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
